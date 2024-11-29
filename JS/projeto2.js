9c94dba2bfcec3daae615750548430c12c17585b
LuisHRodrigues <luishenriquebr505@gmail.com>
2024-11-29 16:16:16 -0300

    .

--- JS/projeto2.js
+++ JS/projeto2.js
@@ -1,14 +1,17 @@
 function reserva() {
   document.getElementById("novaPagina").style.display = "flex";
 }
-function cofirmButton() {
+function confirmButton(event) {
+  event.preventDefault(); // Impede o comportamento padrão do formulário
   alert("Reserva confirmada! Por favor, verifique seu email.");
   document.getElementById("novaPagina").style.display = "none";
-  //Fazendo com que apos o botão de confirmações ser clicado, os inputs fiquem em branco
+  // Fazendo com que após o botão de confirmações ser clicado, os inputs fiquem em branco
   document.getElementById("nome").value = "";
   document.getElementById("email").value = "";
 }
-function cancelButton() {
+
+function cancelButton(event) {
+  event.preventDefault(); // Impede o envio do formulário
   alert("Reserva cancelada!");
   document.getElementById("novaPagina").style.display = "none";
 }
